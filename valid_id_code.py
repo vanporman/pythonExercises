7ac1dd91f77b83190ebb293b66506fac1c13f4cd
Andreas Porman <Andreas Porman>
2017-02-10 17:29:23 +0200

    no improvements, but it worksgit add valid_id_code.py!

--- valid_id_code.py
+++ valid_id_code.py
@@ -15,17 +15,17 @@ def is_person_code_valid(idcode):
     print (idArr)
 
     #arvitab lsiti numbrite summa
-    sumOfIdArr = 0
-    for i in idArr:
-        sumOfIdArr += i
+    #sumOfIdArr = 0
+    #for i in idArr:
+    #    sumOfIdArr += i
     #print (sumOfIdArr)
 
+    #IjaII astme kaalud
     firstgrade = [1,2,3,4,5,6,7,8,9,1]
-    #print (firstgrade)
     secondgrade = [3,4,5,6,7,8,9,1,2,3]
 
     firstgradeweighsum = [a*b for a,b in zip(idArr,firstgrade)]
-    #print (firstgradeweighsum)
+    print (firstgradeweighsum)
     sumoffirstgradeweigh = int(sum(firstgradeweighsum))
     print (sumoffirstgradeweigh)
 
@@ -34,36 +34,41 @@ def is_person_code_valid(idcode):
     calc1 = int(firstgradeweighmodulo*11)
     print (calc1)
     controlnumber1 = int(sumoffirstgradeweigh - calc1)
-    #print ("last number of idcode: ", controlnumber1)
+    print (controlnumber1)
 
     if (controlnumber1 == 10):
+        print ("I astme kontrollnumber tuli 10, alustan II astme kontrolli")
         secondgradeweighsum = [a*b for a,b in zip(idArr, secondgrade)]
-        #print ("SGW: ", secondgradeweighsum)
+        print (secondgradeweighsum)
         sumofsecondgradeweigh = int(sum(secondgradeweighsum))
-        #print ("SGW: ", sumofsecondgradeweigh)
+        print (sumofsecondgradeweigh)
 
         secondgradeweighmodulo = int(sumofsecondgradeweigh/11)
-        #print ("SGW: ", secondgradeweighmodulo)
+        print (secondgradeweighmodulo)
         calc2 = int(secondgradeweighmodulo*11)
-        #print ("SGW: ", calc2)
+        print (calc2)
         controlnumber2 = int(sumofsecondgradeweigh - calc2)
-        #print("SGW: ", controlnumber2)
+        print (controlnumber2)
 
         if (controlnumber2 == 10):
             controlnumber3 = 0
-            print ("2 last number of idcode: ", controlnumber3)
+            print (controlnumber3)
+            print ("II astme kontroll, peab olema null-TRUE: ", controlnumber3)
             return True
         elif (controlnumber2 != idArr[10]):
-            print ("2 this is not id code", controlnumber2)
-            return False
+           print ("II astme kontrolli ei labinud, ei ole idkood-FALSE:", controlnumber2)
+           return False
         else:
-            print ("2 last number of idcode: ", controlnumber2)
+            print ("II astme kontroll-TRUE: ", controlnumber2)
             return True
     elif (controlnumber1 != idArr[10]):
-        print ("1 this is not id code", controlnumber1)
+        print ("I astme kontrolli ei labinud, ei ole idkood-FALSE", controlnumber1)
         return False
     else:
-        print ("1 last number of idcode: ", controlnumber1)
+        print ("I astme kontroll-TRUE: ", controlnumber1)
         return True
 
-is_person_code_valid("49403136526")
+#is_person_code_valid("49403136526")
+#is_person_code_valid("47605030299")
+#is_person_code_valid("37605030299")
+is_person_code_valid("47810016580")
